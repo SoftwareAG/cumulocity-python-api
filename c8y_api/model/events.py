6474e2af3b52a9608d50fde51e83339744aab429
chisou <Christoph.Souris@softwareag.com>
2021-02-03 11:00:41 +0100

    Added Event fragments; Proper handling of updates; More test cases; Added methods delete, delete_by, update, apply_to to Event and Events.

--- c8y_api/model/events.py
+++ c8y_api/model/events.py
@@ -4,28 +4,33 @@
 # Use, reproduction, transfer, publication or disclosure is prohibited except
 # as specifically provided for in your License Agreement with Software AG.
 
-from c8y_api.model._util import _Query, _DatabaseObject, _DatabaseObjectParser
+from c8y_api.model._util import _Query, _UpdatableProperty, _NotUpdatableProperty, \
+    _DatabaseObjectWithFragments, _DatabaseObjectWithFragmentsParser
 
 
-class Event(_DatabaseObject):
+class Event(_DatabaseObjectWithFragments):
 
-    __parser = _DatabaseObjectParser({
+    __parser = _DatabaseObjectWithFragmentsParser({
         'id': 'id',
         'type': 'type',
-        'category': 'category',
         'time': 'time',
-        'creation_time': 'creationTime',
+        '_u_text': 'text',
         # 'source': 'source/id'
-        'text': 'text'})
+        'creation_time': 'creationTime'},
+        ['self', 'source'])
 
-    def __init__(self, c8y=None, type=None, category=None, time=None, source=None, text=None):
+    def __init__(self, c8y=None, type=None, time=None, source=None, text=None):
         super().__init__(c8y=c8y)
         self.type = type
-        self.category = category
         self.time = time
         self.source = source
-        self.text = text
+        self._u_text = text
         self.creation_time = None
+        # trigger update status of defined updatable fields
+        if text:
+            self.text = text
+
+    text = _UpdatableProperty('_u_text')
 
     @classmethod
     def from_json(cls, json_obj):
@@ -34,16 +39,74 @@ class Event(_DatabaseObject):
         obj.id = json_obj['id']
         return obj
 
+    def to_json(self):
+        obj_json = self.__parser.to_full_json(self)
+        if self.source:
+            obj_json['source'] = {'id': self.source}
+        return obj_json
+
+    def to_diff_json(self):
+        obj_json = self.__parser.to_diff_json(self)
+        return obj_json
+
+    def create(self):
+        """
+
+        :return Event:  A fresh Event instance representing what was created
+            within the database. This instance can be used to get at the ID
+            of the new Event object.
+        :see also
+        """
+        self._assert_c8y()
+        result_json = self.c8y.post('/event/events', self.to_json())
+        result = Event.from_json(result_json)
+        result.c8y = self.c8y
+        return result
+
+    def update(self):
+        self._assert_c8y()
+        self._assert_id()
+        result_json = self.c8y.put('/event/events/' + self.id, self.to_diff_json())
+        result = Event.from_json(result_json)
+        result.c8y = self.c8y
+        return result
+
+    def apply_to(self, other_id):
+        """Apply the changed made to this object to another object in the database.
+
+
+
+        :param
+        """
+        self._assert_c8y()
+        # if no field was updated, apparently the entire object should
+        # be applied, so we signal updates ourselves by touching all updatable fields
+        # put diff json to another object (by ID)
+        result_json = self.c8y.put('/event/events/' + other_id, self.to_diff_json())
+        result = Event.from_json(result_json)
+        result.c8y = self.c8y
+        return result
+
+    def delete(self):
+        self._assert_c8y()
+        self._assert_id()
+        self.c8y.delete('/event/events/' + self.id)
+
 
 class Events(_Query):
 
     def __init__(self, c8y):
-        super().__init__(c8y, 'event/events')
+        super().__init__(c8y, '/event/events')
 
-    def select(self, type=None, category=None, source=None, fragment=None, # noqa (type)
+    def get(self, event_id):
+        event_object = Event.from_json(self._get_object(event_id))
+        event_object.c8y = self.c8y  # inject c8y connection into instance
+        return event_object
+
+    def select(self, type=None, source=None, fragment=None, # noqa (type)
                before=None, after=None, min_age=None, max_age=None, reverse=False,
                limit=None, page_size=1000):
-        base_query = self._build_base_query(type=type, category=category, source=source, fragment=fragment,
+        base_query = self._build_base_query(type=type, source=source, fragment=fragment,
                                             before=before, after=after, min_age=min_age, max_age=max_age,
                                             reverse=reverse, page_size=page_size)
         page_number = 1
@@ -62,3 +125,54 @@ class Events(_Query):
             except StopIteration:
                 break
             page_number = page_number + 1
+
+    def get_all(self, type=None, source=None, fragment=None, # noqa (type)
+               before=None, after=None, min_age=None, max_age=None, reverse=False,
+               limit=None, page_size=1000):
+        return [x for x in self.select(type=type, source=source, fragment=fragment,
+                                       before=before, after=after, min_age=min_age, max_age=max_age, reverse=reverse,
+                                       limit=limit, page_size=page_size)]
+
+    def create(self, *events):
+        """Create event objects within the database.
+
+        :param events:  collection of Event instances
+        :returns:  None
+        """
+        for event in events:
+            self.c8y.post('/event/events', event.to_json())
+
+    def update(self, *events):
+        for event in events:
+            self.c8y.put('/event/events/' + event.id, event.to_diff_json())
+
+    def apply_to(self, event, *event_ids):
+        """"""
+        diff_json = event.to_diff_json()
+        for eid in event_ids:
+            self.c8y.put('/event/events/' + eid, diff_json)
+
+    def delete(self, *events):
+        """Delete one or more events.
+
+        The events can be specified as instances of Event (then, the id field
+        needs to be defined) or simply as ID (integers or strings).
+        :param events:  events objects within Cumulocity specified as Event
+            instances or by ID
+        :returns:  None
+        """
+        try:
+            event_ids = [e.id for e in events]
+        except AttributeError:
+            event_ids = events
+        for eid in event_ids:
+            self.c8y.delete('/event/events/' + eid)
+
+    def delete_by(self, type=None, source=None, fragment=None,  # noqa (type)
+                  before=None, after=None, min_age=None, max_age=None):
+        # build a base query
+        base_query = self._build_base_query(type=type, source=source, fragment=fragment,
+                                            before=before, after=after, min_age=min_age, max_age=max_age)
+        # remove &page_number= from the end
+        query = base_query[:base_query.rindex('&')]
+        self.c8y.delete(query)
--- sample/events.py
+++ sample/events.py
@@ -4,17 +4,22 @@
 # Use, reproduction, transfer, publication or disclosure is prohibited except
 # as specifically provided for in your License Agreement with Software AG.
 
+import datetime as dt
+
 from c8y_api.app import CumulocityApi
-from c8y_api.model import Events
+from c8y_api.model import Event
 
 
 def print_event(event):
     print(f'\nEvent #{event.id}, {event.time}')
     print(f'  "{event.text}"')
     print(f'  Type:     {event.type}')
-    print(f'  Category: {event.category}')
     print(f'  Source:   {event.source}')
     print(f'  Created:  {event.creation_time}')
+    fragment_names = list(event.fragments.keys())
+    print(f'  Fragments: {", ".join(fragment_names)}')
+    for key in fragment_names:
+        print(f'   {key}: {event.fragments[key]}')
 
 
 c8y = CumulocityApi()
@@ -27,11 +32,84 @@ for e in c8y.events.select(type='Local Config Changed', limit=10):
         sample_source_id = e.source
     print_event(e)
 
-# (1) select by category
-for e in c8y.events.select(category='Device Maintenance', limit=10):
-    print_event(e)
-
-# (1) select by source
+# (2) select by source
 for e in c8y.events.select(source=sample_source_id, limit=10):
     print_event(e)
 
+# (3) create a new event from scratch
+date_string = dt.datetime.now().isoformat()
+e1 = Event(c8y=c8y, type='Custom Type', source=sample_source_id, text='Some custom text.', time=date_string)
+e1.add_fragment("custom_fragment", key="value", more={"x": 1, "y": 2})
+e1_created = e1.create()  # the create method returns a new object, having the Id defined
+print(f"\nEvent created: #{e1_created.id}")
+print(f"  Custom fragment: {e1_created.custom_fragment.more}")
+
+# (4) read events by ID
+e1_read = c8y.events.get(e1_created.id)
+assert e1_read.id == e1_created.id
+
+# (5) delete an event
+e1_read.delete()
+try:
+    c8y.events.get(e1_read.id)
+    raise AssertionError("An exception should have been thrown.")
+except KeyError:
+    print(f"\nEvent deleted: #{e1_read.id}")
+
+# (6) bulk create
+print("\nCreating multiple events ...")
+c8y.events.create(e1, e1, e1)  # 3 times the same event created, IDs are unknown
+
+# (7) use get_all to get all matching as a list
+events_so_far = c8y.events.get_all(type="Custom Type")
+for e in events_so_far:
+    print(f"  Matching event: #{e.id}")
+assert len(events_so_far) > 3  # it could be more form previous tests
+
+# (8) delete events by query
+c8y.events.delete_by(type='Custom Type')
+assert len(c8y.events.get_all(type='Custom Type')) == 0
+print("\nDeleted all events with type='Custom Type'.")
+
+# (9) delete multiple events by their ID
+id1 = e1.create().id
+id2 = e1.create().id
+print(f"\nAbout to delete fresh created events #{id1} and #{id2} ...")
+c8y.events.delete(id1, id2)
+assert len(c8y.events.get_all(type='Custom Type')) == 0
+print("Done.")
+
+# (10) create and update an event
+e2 = Event(c8y=c8y, type='Custom Type', source=sample_source_id, text='Some custom text.', time=date_string)\
+    .create()
+print(f"\nCreated event #{e2.id}.")
+print(f"  Diff JSON: {e2.to_diff_json()}")
+e2.text = "Updated event text."
+e2.type = 'Updated Type'  # Updating the type is not supported by Cumulocity, this will be ignored
+print(f"  Diff JSON after changes: {e2.to_diff_json()}")
+e2 = e2.update()  # this will only send the changed fields for best performance
+print(f"Event #{e2.id} updated.")
+e2a = c8y.events.get(e2.id)
+assert e2a.type != 'Updated Type'
+assert e2a.text == "Updated event text."
+
+# (10a) it is also possible to apply an update to other objects in the database
+updater = Event(c8y=c8y, text='Updated event text #2.')
+print(f"\nApplying update to event #{e2.id}: {updater.to_diff_json()}")
+e2 = updater.apply_to(e2.id)
+e2b = c8y.events.get(e2.id)
+assert e2b.text == "Updated event text #2."
+
+# (10b) the same can be done on multiple IDs
+e3 = Event(c8y=c8y, type="Other", time=date_string, source=sample_source_id, text="Other")\
+    .create()
+updater2 = Event(c8y=c8y, text='Updated event text #3.')
+c8y.events.apply_to(updater2, e2.id, e3.id)
+e2c = c8y.events.get(e2.id)
+e3c = c8y.events.get(e3.id)
+assert e2c.text == "Updated event text #3."
+assert e2c.text == e3c.text
+
+# cleanup
+e2.delete()
+e3.delete()
