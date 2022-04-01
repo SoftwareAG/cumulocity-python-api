eb279b8dd30483b416f3634ef12d50d2849aa103
Christoph Souris <christoph.souris@softwareag.com>
2022-04-01 16:44:35 +0200

    Code cleanup to meet naming conventions; added delete_by; added creation_date; fixed fragment handling; Added Status constants.

--- c8y_api/model/operations.py
+++ c8y_api/model/operations.py
@@ -6,13 +6,13 @@
 
 from __future__ import annotations
 
+from datetime import datetime, timedelta
 from typing import Type, List, Generator
 
 from c8y_api._base_api import CumulocityRestApi
 
-from c8y_api.model._base import CumulocityResource, ComplexObject
+from c8y_api.model._base import CumulocityResource, ComplexObject, SimpleObject
 from c8y_api.model._parser import ComplexObjectParser
-from c8y_api.model._base import _DictWrapper
 from c8y_api.model._util import _DateUtil
 
 
@@ -26,19 +26,29 @@ class Operation(ComplexObject):
     See also: https://cumulocity.com/api/10.11.0/#tag/Operations
     """
 
+    class Status:
+        """Operation statuses."""
+        PENDING = 'PENDING'
+        EXECUTING = 'EXECUTING'
+        SUCCESSFUL = 'SUCCESSFUL'
+        FAILED = 'FAILED'
+
     # these need to be defined like this for the abstract super functions
     _resource = '/devicecontrol/operations'
-    _parser = ComplexObjectParser({'deviceId': 'deviceId', 'description': 'description', 'status': 'status'}, [])
-    # _accept
-    # _not_updatable
+    _parser = ComplexObjectParser({
+        'device_id': 'deviceId',
+        'creation_time': 'creationTime',
+        '_u_description': 'description',
+        '_u_status': 'status'}, [])
+    _accept = 'application/vnd.com.nsn.cumulocity.operation+json'
 
-    def __init__(self, c8y=None, deviceId=None, description=None, status=None, **kwargs):
+    def __init__(self, c8y=None, device_id=None, description=None, status=None, **kwargs):
         """ Create a new Operation object.
 
         Params:
-            c8y(CumulocityRestApi):  Cumulocity connection reference; needs
+            c8y (CumulocityRestApi):  Cumulocity connection reference; needs
                 to be set for direct manipulation (create, delete)
-            deviceId(str):  Device ID which this operation is for 
+            device_id (str):  Device ID which this operation is for
             kwargs:  All additional named arguments are interpreted as
                 custom fragments e.g. for data points.
 
@@ -46,10 +56,22 @@ class Operation(ComplexObject):
             Operation object
         """
         super().__init__(c8y, **kwargs)
-        self.deviceId = deviceId
-        self.description = description
-        self.status = status
+        self.device_id = device_id
+        self.creation_time = None
+        self._u_description = description
+        self._u_status = status
 
+    description = SimpleObject.UpdatableProperty('_u_description')
+    status = SimpleObject.UpdatableProperty('_u_status')
+
+    @property
+    def creation_datetime(self) -> datetime:
+        """Convert the operation's creation time to a Python datetime object.
+
+        Returns:
+            Standard Python datetime object for the operation's creation time.
+        """
+        return super()._to_datetime(self.creation_time)
 
     @classmethod
     def from_json(cls, json) -> Operation:
@@ -59,8 +81,8 @@ class Operation(ComplexObject):
         Cumulocity REST API.
 
         Params:
-            operation_json(dict):  JSON object (nested dictionary)
-                representing a operation within Cumulocity
+            json (dict):  JSON object (nested dictionary)
+                representing an operation within Cumulocity
 
         Returns:
             Operation object
@@ -72,20 +94,13 @@ class Operation(ComplexObject):
         # (no doc update required)
         # creation time is always excluded
         obj_json = super()._to_json(only_updated, exclude={'creation_time'})
-        # obj_json['status'] = self.status
-        print('Test')
-        obj_json['status'] = self.status
-        obj_json['description'] = self.description
-        # source needs to be set manually, but it cannot be updated
-        if not only_updated and self.deviceId:
-            obj_json['deviceId'] = self.deviceId
         return obj_json
 
-    # the __getattr__ function is overwritten to return a wrapper that doesn't signal updates
-    # (because Measurements are not updated, can only be created from scratch)
-    def __getattr__(self, item):
-        return _DictWrapper(self.fragments[item], on_update=None)
-
+    # # the __getattr__ function is overwritten to return a wrapper that doesn't signal updates
+    # # (because Measurements are not updated, can only be created from scratch)
+    # def __getattr__(self, item):
+    #     return _DictWrapper(self.fragments[item], on_update=None)
+    #
     @property
     def datetime(self) -> Type[datetime] | None:
         """ Convert the measurement's time to a Python datetime object.
@@ -108,7 +123,6 @@ class Operation(ComplexObject):
     def update(self) -> Operation:
         """Update the Operation within the database."""
         return super()._update()
-
 
 
 class Operations(CumulocityResource):
@@ -139,9 +153,11 @@ class Operations(CumulocityResource):
         operation.c8y = self.c8y  # inject c8y connection into instance
         return operation
 
-    def select(self, agentId=None, deviceId=None, status=None, fragment=None,
-               before=None, after=None, min_age=None, max_age=None, reverse=False,
-               limit=None, page_size=1000) -> Generator[Operation]:
+    def select(self, agent_id: str = None, device_id: str = None, status: str = None,
+               bulk_id: str = None, fragment: str = None,
+               before: str | datetime = None, after: str | datetime = None,
+               min_age: timedelta = None, max_age: timedelta = None,
+               reverse: bool = False, limit: int = None, page_size: int = 1000) -> Generator[Operation]:
         """ Query the database for operations and iterate over the results.
 
         This function is implemented in a lazy fashion - results will only be
@@ -152,9 +168,10 @@ class Operations(CumulocityResource):
         combined (within reason).
 
         Params:
-            agentId (str): Database ID of agent
-            deviceId (str):  Database ID of device
+            agent_id (str): Database ID of agent
+            device_id (str):  Database ID of device
             status (str): Status of operation
+            bulk_id (str): The bulk operation ID that this object belongs to
             fragment (str):  Name of a present custom/standard fragment
             before (datetime|str):  Datetime object or ISO date/time string.
                 Only operaton assigned to a time before this date are
@@ -176,15 +193,17 @@ class Operations(CumulocityResource):
         Returns:
             Generator[Operation]: Iterable of matching Operation objects
         """
-        base_query = self._build_base_query(agentId=agentId, deviceId=deviceId, status=status, fragment=fragment,
+        base_query = self._build_base_query(agent_id=agent_id, device_id=device_id, status=status, bulk_id=bulk_id,
+                                            fragment=fragment,
                                             before=before, after=after, min_age=min_age, max_age=max_age,
                                             reverse=reverse, page_size=page_size)
         return super()._iterate(base_query, limit, Operation.from_json)
 
-    def get_all(self, agentId=None, deviceId=None, status=None,  # noqa (type)
-                fragment=None,  value=None, series=None,
-                before=None, after=None, min_age=None, max_age=None, reverse=False,
-                limit=None, page_size=1000) -> List[Operation]:
+    def get_all(self, agent_id: str = None, device_id: str = None, status: str = None,
+                bulk_id: str = None, fragment: str = None,
+                before: str | datetime = None, after: str | datetime = None,
+                min_age: timedelta = None, max_age: timedelta = None,
+                reverse: bool = False, limit: int = None, page_size: int = 1000) -> List[Operation]:
         """ Query the database for operations and return the results
         as list.
 
@@ -194,12 +213,13 @@ class Operations(CumulocityResource):
         Returns:
             List of matching Measurement objects
         """
-        return list(self.select(agentId=agentId, deviceId=deviceId, status=status,
+        return list(self.select(agent_id=agent_id, device_id=device_id, status=status, bulk_id=bulk_id,
                                 fragment=fragment, before=before, after=after, min_age=min_age, max_age=max_age,
                                 reverse=reverse, limit=limit, page_size=page_size))
 
-    def get_last(self, agentId=None, deviceId=None, status=None, fragment=None, value=None, series=None,  # noqa (type)
-                 before=None, min_age=None) -> Operation:
+    def get_last(self, agent_id: str = None, device_id: str = None, status: str = None,
+                 bulk_id: str = None, fragment: str = None,
+                 before: str | datetime = None, min_age: timedelta = None) -> Operation:
         """ Query the database and return the last matching operation.
 
         This function is a special variant of the select function. Only
@@ -213,10 +233,44 @@ class Operations(CumulocityResource):
         after = None
         if not before and not min_age:
             after = '1970-01-01'
-        base_query = self._build_base_query(agentId=agentId, deviceId=deviceId, status=status,
-                                            fragment=fragment, after=after,
+        base_query = self._build_base_query(agent_id=agent_id, device_id=device_id, status=status,
+                                            bulk_id=bulk_id, fragment=fragment, after=after,
                                             before=before, min_age=min_age, reverse=True, page_size=1)
         m = Operation.from_json(self._get_page(base_query, "1")[0])
         m.c8y = self.c8y  # inject c8y connection into instance
         return m
 
+    def delete_by(self, agent_id: str = None, device_id: str = None, status: str = None,
+                  bulk_id: str = None, fragment: str = None,
+                  before: str | datetime = None, after: str | datetime = None,
+                  min_age: timedelta = None, max_age: timedelta = None):
+        """Query the database and delete matching operations.
+
+        All parameters are considered to be filters, limiting the result set
+        to objects which meet the filters specification. Filters can be
+        combined (as defined in the Cumulocity REST API).
+
+        Params:
+            agent_id (str): Database ID of agent
+            device_id (str):  Database ID of device
+            status (str): Status of operation
+            bulk_id (str): The bulk operation ID that this object belongs to
+            fragment (str):  Name of a present custom/standard fragment
+            before (datetime|str):  Datetime object or ISO date/time string.
+                Only operaton assigned to a time before this date are
+                returned.
+            after (datetime|str):  Datetime object or ISO date/time string.
+                Only operation assigned to a time after this date are
+                returned.
+            min_age (timedelta):  Timedelta object. Only operation of
+                at least this age are returned.
+            max_age (timedelta):  Timedelta object. Only operations with
+                at most this age are returned.
+                # build a base query
+        """
+        base_query = self._build_base_query(agent_id=agent_id, device_id=device_id, status=status,
+                                            bulk_id=bulk_id, fragment=fragment,
+                                            before=before, after=after, min_age=min_age, max_age=max_age)
+        # remove &page_number= from the end
+        query = base_query[:base_query.rindex('&')]
+        self.c8y.delete(query)
