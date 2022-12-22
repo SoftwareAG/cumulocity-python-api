8c6f6af4b285ce3e46a830d8787eda37d9c8899a
Christoph Souris <github@whisperingvoices.net>
2022-12-22 17:37:39 +0100

    Adding microservice handling utilities.

--- tasks.py
+++ tasks.py
@@ -7,6 +7,8 @@
 from invoke import task
 from setuptools_scm import get_version
 
+import util.microservice_util as ms_util
+
 
 @task
 def show_version(_):
@@ -43,13 +45,53 @@ def build(c):
     "version": "Microservice version. Defaults to '1.0.0'.",
 })
 def build_ms(c, sample, version='1.0.0'):
-    """Build a Cumulocity micro service.
+    """Build a Cumulocity microservice binary for upload.
 
-    This will build a ready to deploy Cumulocity micro service from a sample
+    This will build a ready to deploy Cumulocity microservice from a sample
     file within the `samples` folder. Any sample Python script can be used
-    (if it implements micro service logic).
+    (if it implements microservice logic).
 
     Use the file name without .py extension as name. The build microservice
     will use a similar name, following Cumulocity naming guidelines.
     """
-    c.run(f'samples/build.sh {sample} {version}')
+    sample_name = ms_util.format_sample_name(sample)
+    c.run(f'samples/build.sh {sample_name} {version}')
+
+
+@task(help={
+    'sample': "Which sample to register."
+})
+def register_ms(c, sample):
+    """Register a sample as microservice at Cumulocity."""
+    ms_util.register_microservice(ms_util.format_sample_name(sample))
+
+
+@task(help={
+    'sample': "Which sample to unregister."
+})
+def unregister_ms(c, sample):
+    """Unregister a sample microservice from Cumulocity."""
+    ms_util.unregister_microservice(ms_util.format_sample_name(sample))
+
+
+@task(help={
+    'sample': "Which sample to register."
+})
+def get_credentials(c, sample):
+    """Unregister a sample microservice from Cumulocity."""
+    user, password = ms_util.get_credentials(ms_util.format_sample_name(sample))
+    print(f"Username:  {user}\n"
+          f"Password:  {password}")
+
+
+@task(help={
+    'sample': "Which sample to create a .env file for."
+})
+def create_env(c, sample):
+    """Create a sample specific .env-{sample_name} file using the
+    credentials of a corresponding microservice registered at Cumulocity."""
+    sample_name = ms_util.format_sample_name(sample)
+    user, password = ms_util.get_credentials(sample_name)
+    with open(f'.env-{sample_name}', 'w') as f:
+        f.write(f"C8Y_USER={user}\n"
+                f"C8Y_PASSWORD={password}\n")
